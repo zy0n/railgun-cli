45a2d6610b56020ff6ef3f8071211937de363940
zy0n.bear <zy0n@users.noreply.github.com>
2023-02-20 20:59:37 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# railgun-cli
- Railgun Wallet Command Line Interface
+# <p style="text-align: center; font-size: 88px">railgun-cli</p>
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Contributing](../CONTRIBUTING.md)
+
+## About <a name = "about"></a>
+
+This is a basic implementation for pulling Transaction History and current RAILGUNB balances from the RAILGUN Relays. 
+
+
+🍻 A full transaction history will be saved locally for double checking your balances and transactions. 
+
+⛔️ No transactions will be made!
+
+## Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+What things you need to install the software and how to install them.
+
+```
+NodeJS
+npm (or) yarn
+```
+
+### Installing
+
+Some basic instructions on how to get the project operational.
+
+##### Clone the repo:
+
+```sh
+git clone https://github.com/zy0n/railgun-cli
+```
+##### Enter the Repo
+```sh
+cd railgun-cli
+```
+##### Install Dependencies:
+```
+npm install
+or
+yarn install
+```
+
+End with an example of getting some data out of the system or using it for a little demo.
+
+## Usage <a name = "usage"></a>
+
+Okay so its relatively simple. You only need your mnemonic.
+#####Usage Flags:
+```sh
+
+ --mnemonic 'YOUR_MNEMONIC_PHRASE' # Mnemonic Must be quoted.
+ --pass SECRET_PASSWORD
+ --chain ethereum #OPTIONALPARAM| 
+ # Default: ethereum    - Choices: [ethereum, polygon] 
+
+```
+Example Usage:
+**Do NOT load funds into the example mnemonic. This was pulled from the RAILGUN mocks. It's a good example to use here, as it has available transaction history on polygon to view.**
+
+*Be sure make sure you're in the main directory.*
+#####yarn
+```sh
+yarn start --mnemonic 'pause crystal tornado alcohol genre cement fade large song like bag where' --pass SomethingSecret --chain polygon
+```
+
+#####node
+```sh
+node src/railgun-cli.js --mnemonic 'pause crystal tornado alcohol genre cement fade large song like bag where' --pass SomethingSecret --chain polygon
+```
+
+
+## Todo: <a name = "todo"></a>
+- [ ] Get the binaries to build properly.
+- [ ] Add support for the rest of the available chains.
+- [ ] Add support for loading existing stored wallets.
+- [ ] Add support for handling transactions ? 
+[ 🎱 ] Suggestions??
+
+######As of right now building with PKG is failing to produce a running executable. Which in reality, i didnt want to provide executables anyway, but its nice for you to build them still!
+
+#Feel free to fork & contribute!
+
+###🍻 Cheers for Donations!
+*If you like my work and would like to help me keep on keepin' on those options are below. Thanks!!*
+Definitely not a requirement 💋
+👁️ Public (unshielded): 
+```sh
+0xD74b78AA69d9ee3e232beEE935025530E4d7080d
+```
+🛡️ Private (shielded):
+```sh
+0zk1qywazlck3dsmxkhx7vswfkf0zyuhguna79f87cpqad6wlu3d74plrrv7j6fe3z53laqmr4aeh35unfg67etsksd80qj2pvf9r6egpyyhnh56qe9nntmevq6yu6u
+```
---
+++ src/railgun-cli.js
@@ -0,0 +1,186 @@
+const fs = require("fs");
+const crypto = require("crypto");
+const { BigNumber, utils } = require("ethers");
+const LevelDOWN = require("leveldown");
+var argv = require("minimist")(process.argv.slice(2));
+
+const {
+  ArtifactStore,
+  loadProvider,
+  startRailgunEngine,
+  createRailgunWallet,
+  fullWalletForID,
+  getWalletTransactionHistory,
+  setOnBalanceUpdateCallback,
+  closeRailgunEngine,
+} = require("@railgun-community/quickstart");
+
+const { ChainType } = require("@railgun-community/shared-models");
+
+const RAILGUN_DB = ".railgun.db";
+
+const networkConfig = {
+  ethereum: {
+    name: "Ethereum",
+    type: ChainType.EVM,
+    chainId: 1,
+    providers: [
+      {
+        provider: "https://rpc.ankr.com/eth",
+        priority: 1,
+        weight: 1,
+      },
+      {
+        provider: "https://cloudflare-eth.com/",
+        priority: 2,
+        weight: 1,
+      },
+    ],
+  },
+  polygon: {
+    name: "Polygon",
+    type: ChainType.EVM,
+    chainId: 137,
+    providers: [
+      {
+        provider: "https://rpc.ankr.com/polygon",
+        priority: 1,
+        weight: 1,
+      },
+      {
+        provider: "https://polygon-rpc.com",
+        priority: 2,
+        weight: 1,
+      },
+      {
+        provider: "https://rpc-mainnet.maticvigil.com",
+        priority: 3,
+        weight: 1,
+      },
+    ],
+  },
+};
+
+function serializeObject(obj) {
+  for (let prop in obj) {
+    if (typeof obj[prop] === "object") {
+      serializeObject(obj[prop]);
+    } else {
+      obj[prop] = `${obj[prop]}`;
+    }
+  }
+  return obj;
+}
+
+function generateReport(items) {
+  return items?.map(item => {
+    const output = {}
+    output.txid = item.txid;
+    item.transferERC20Amounts.length > 0 ? output.transferERC20Amounts = item.transferERC20Amounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
+    item.changeERC20Amounts.length > 0 ? output.changeERC20Amounts = item.changeERC20Amounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
+    item.receiveERC20Amounts.length > 0 ? output.receiveERC20Amounts = item.receiveERC20Amounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
+    item.unshieldERC20Amounts.length > 0 ? output.unshieldERC20Amounts = item.unshieldERC20Amounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
+    item.receiveNFTAmounts.length > 0 ? output.receiveNFTAmounts = item.receiveNFTAmounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
+    item.transferNFTAmounts.length > 0 ? output.transferNFTAmounts = item.transferNFTAmounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
+    item.unshieldNFTAmounts.length > 0 ? output.unshieldNFTAmounts = item.unshieldNFTAmounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
+    output.version = item.version;
+    return output;
+  })
+}
+
+async function initializeEngine(chainName) {
+  const engineDb = new LevelDOWN(RAILGUN_DB);
+  const artifactStorage = new ArtifactStore(
+    fs.promises.readFile,
+    async (dir, path, data) => {
+      await fs.promises.mkdir(dir, { recursive: true });
+      await fs.promises.writeFile(path, data);
+    },
+    fs.promises.fileExists
+  );
+  // initialize engine
+  await startRailgunEngine(
+    "railguncli",
+    engineDb,
+    false,
+    artifactStorage,
+    false,
+    false
+  );
+  // initialize network
+  await loadProvider(
+    networkConfig[chainName],
+    networkConfig[chainName].name,
+    false
+  );
+
+  return {
+    type: networkConfig[chainName].type,
+    id: networkConfig[chainName].chainId,
+  };
+}
+
+async function initializeWallet(password, mnemonic) {
+  const salt = "Always Salt your Pizza!!👀";
+  const secretBytes = crypto
+    .createHash("sha256")
+    .update(`${salt}:${password}`)
+    .digest("hex");
+  return await createRailgunWallet(secretBytes, mnemonic);
+}
+
+async function fetchHistory(walletInfo, chainInfo) {
+
+  console.log(`RAILGUN zkAddress: ${walletInfo.railgunAddress}`)
+  console.log("Fetching Transaction History 👀");
+  const wallet = fullWalletForID(walletInfo.id);
+  const balances = await wallet.balances(chainInfo);
+  setOnBalanceUpdateCallback(async (tokenBalances) => {
+    const _txHistory = await getWalletTransactionHistory(chainInfo, wallet.id);
+    const txHistory = generateReport(_txHistory.items);
+    console.log("🎱 Transactions Found:", _txHistory.items?.length);
+
+    const fullTxData = serializeObject(balances);
+    const outFile = JSON.stringify(
+      { tokenBalances, txHistory, fullTxData },
+      null,
+      2
+    );
+    fs.writeFileSync("txdata.json", outFile);
+    console.log(`Full report saved to: ${process.cwd()}/txdata.json`);
+    closeRailgunEngine();
+    
+    process.exit(0);
+  });
+}
+
+async function main() {
+  console.log("\n");
+  if (!argv.mnemonic) {
+    console.log(
+      `Error:\nPlease enter a mnemonic using flag --mnemonic 'mnemonic phrase'\n\nExample: railgun-cli --pass secretpassword --mnemonic='dog fish cat duck turkey'\n\n`
+    );
+    return;
+  }
+  const mnemonic = argv.mnemonic;
+  if (!argv.pass) {
+    console.log(
+      `Error:\nPlease enter a password using flag --pass [password]\n\nExample: railgun-cli --pass secretpassword --mnemonic='dog fish cat duck turkey'\n\n`
+    );
+    return;
+  }
+  const password = argv.pass;
+  let chain = argv.chain || "ethereum";
+  const currentChain = await initializeEngine(chain);
+  const { railgunWalletInfo, error } = await initializeWallet(
+    password,
+    mnemonic
+  );
+  if(error){
+    console.log("💥 Error Initializing.");
+    process.exit(0);
+    return;
+  }
+  await fetchHistory(railgunWalletInfo, currentChain);
+}
+main();

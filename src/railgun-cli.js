6fc8cf266cd81401d9fb3350f717a601eb08d19a
zy0n.bear <zy0n@users.noreply.github.com>
2023-02-20 21:20:22 -0500

    Refactor generate report

--- src/railgun-cli.js
+++ src/railgun-cli.js
@@ -72,17 +72,22 @@ function serializeObject(obj) {
   return obj;
 }
 
+const formatAmount = (obj) => {
+  obj.amountString = utils.formatEther(BigNumber.from(obj.amountString));
+  return obj;
+}
+
 function generateReport(items) {
   return items?.map(item => {
     const output = {}
     output.txid = item.txid;
-    item.transferERC20Amounts.length > 0 ? output.transferERC20Amounts = item.transferERC20Amounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
-    item.changeERC20Amounts.length > 0 ? output.changeERC20Amounts = item.changeERC20Amounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
-    item.receiveERC20Amounts.length > 0 ? output.receiveERC20Amounts = item.receiveERC20Amounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
-    item.unshieldERC20Amounts.length > 0 ? output.unshieldERC20Amounts = item.unshieldERC20Amounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
-    item.receiveNFTAmounts.length > 0 ? output.receiveNFTAmounts = item.receiveNFTAmounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
-    item.transferNFTAmounts.length > 0 ? output.transferNFTAmounts = item.transferNFTAmounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
-    item.unshieldNFTAmounts.length > 0 ? output.unshieldNFTAmounts = item.unshieldNFTAmounts.map(tea=>{tea.amountString = utils.formatEther(BigNumber.from(tea.amountString)); return tea}) : null;
+    item.transferERC20Amounts.length > 0 ? output.transferERC20Amounts = item.transferERC20Amounts.map(formatAmount) : null;
+    item.changeERC20Amounts.length > 0 ? output.changeERC20Amounts = item.changeERC20Amounts.map(formatAmount) : null;
+    item.receiveERC20Amounts.length > 0 ? output.receiveERC20Amounts = item.receiveERC20Amounts.map(formatAmount) : null;
+    item.unshieldERC20Amounts.length > 0 ? output.unshieldERC20Amounts = item.unshieldERC20Amounts.map(formatAmount) : null;
+    item.receiveNFTAmounts.length > 0 ? output.receiveNFTAmounts = item.receiveNFTAmounts.map(formatAmount) : null;
+    item.transferNFTAmounts.length > 0 ? output.transferNFTAmounts = item.transferNFTAmounts.map(formatAmount) : null;
+    item.unshieldNFTAmounts.length > 0 ? output.unshieldNFTAmounts = item.unshieldNFTAmounts.map(formatAmount) : null;
     output.version = item.version;
     return output;
   })
